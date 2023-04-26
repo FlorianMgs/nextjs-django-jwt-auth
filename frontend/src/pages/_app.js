6fc19e65ed3c1059d4f5ec26f8c2fa7127f6e683
FlorianMgs <madgic.shrooms@gmail.com>
2023-04-26 08:05:24 +0700

    first commit

---
+++ frontend/src/pages/_app.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import { Provider } from 'react-redux'
+import { useStore } from '../store'
+
+export default function App({ Component, pageProps }) {
+  const store = useStore(pageProps.initialReduxState)
+
+  return (
+    <Provider store={store}>
+      <Head>
+        <link
+          href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css'
+          rel='stylesheet'
+          integrity='sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ'
+          crossorigin='anonymous'
+        ></link>
+        <script
+          src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js'
+          integrity='sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe'
+          crossorigin='anonymous'
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </Provider>
+  )
+}
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,9 @@
+const homePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default homePage
